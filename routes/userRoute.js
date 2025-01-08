1c9b81024a700cf131809e1064a7875741fb7e53
SK-x7 <111245838+SK-x7@users.noreply.github.com>
2025-01-08 17:51:50 +0530

    fixed api url for new endpoints

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require("../controllers/userController")
 
 
-router.route("/:username").get(userController.checkUserExists).post(userController.createUser);
+
 router.route("/signup").post(userController.createUser);
 router.route("/").post(userController.loginUser);
 
